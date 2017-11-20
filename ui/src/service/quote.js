a335704cb338765b81d2cff48478d2018fe444ba
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-11-20 21:36:14 +0800

    add gh-pages

--- ui/src/service/quote.js
+++ ui/src/service/quote.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const URL = "https://enigmatic-brushlands-20140.herokuapp.com";
-const URL = "http://127.0.0.1:8000";
+const URL = "https://enigmatic-brushlands-20140.herokuapp.com";
 
 async function fetchRandomQuote() {
     const randomQuote = await axios.get(URL + "/quote/random").then(res => res.data);
