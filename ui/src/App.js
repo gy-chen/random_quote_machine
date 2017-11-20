18cd5a6bcc0d20af2496aefebd754b26edc97300
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-11-20 21:28:06 +0800

    add navbar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,46 +1,15 @@
 import React, {Component} from 'react';
-import Quote from './Quote';
-import {fetchRandomQuote} from './service/quote';
+import Navbar from './Navbar';
+import QuoteApp from './QuoteApp';
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            quote: null
-        };
-    }
-
-    componentDidMount() {
-        this._fetchRandomQuote();
-    }
-
-    async _fetchRandomQuote() {
-        const quote = await fetchRandomQuote();
-        this.setState({
-            quote
-        });
-    }
-
-    _renderQuote() {
-        if (!this.state.quote) {
-            return;
-        }
-        const {quote: {quote}} = this.state.quote;
-        const {video: {title, url}} = this.state.quote;
-        return (
-            <Quote quote={quote} title={title} url={url} onRefresh={() => this._fetchRandomQuote()}/>
-        );
-    }
-
     render() {
         return (
-            <div className="container d-flex justify-content-center h-100">
-                <div className="row justify-content-center align-self-center">
-                    <div className="col-md-6 col-sm-12">
-                        {this._renderQuote()}
-                    </div>
+            <div>
+                <Navbar/>
+                <div className="mt-4">
+                    <QuoteApp/>
                 </div>
             </div>
         );
